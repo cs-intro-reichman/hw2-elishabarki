87623c2f0d9827355fb03b9e4d2c6effb40693c7
elishabarki <elisha.barki@gmail.com>
2024-11-18 22:44:21 +0200

    Update Cheers.java
    after putting spaces

--- Cheers.java
+++ Cheers.java
@@ -13,7 +13,7 @@ public class Cheers {
             if (lettersForAn.indexOf(letter) != -1) {
                 System.out.println("Give me an " + letter + ": " + letter + "!");
             } else {
-                System.out.println("Give me a " + letter + ": " + letter + "!");
+                System.out.println("Give me a  " + letter + ": " + letter + "!");
             } 
         }       
 
